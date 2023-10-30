1e73f61b2fbfa3f6f981400691b915cd76fdb3a5
CharaD7 <jijakahn6@gmail.com>
2023-10-30 15:13:04 +0000

    Worked on basic match task 2

---
+++ src/bin/a4b.rs
@@ -0,0 +1,23 @@
+// Topic: Decision making with match
+//
+// Program requirements:
+// * Display "one", "two", "three", or "other" based on whether 
+// the value of variable is 1, 2, 3, or some other number,
+// respectively
+//
+// Notes:
+// * Use a variable set to any integer
+// * Use a match expression to determine which message to display
+// * Use an undersscore (_) to match on any value
+
+fn main () {
+    let num_of_oranges = 3;
+
+    match num_of_oranges {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other"),
+    }
+}
+
