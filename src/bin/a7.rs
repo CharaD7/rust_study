178ca561c560e37c85450acbbc14060e170104bb
CharaD7 <jijakahn6@gmail.com>
2023-11-01 17:32:25 +0000

    Worked on enum task

---
+++ src/bin/a7.rs
@@ -0,0 +1,53 @@
+// Topic: Working with an enum
+//
+// Program requirements:
+// * Prints the name of a color to the terminal.
+//
+// Notes:
+// * Use an enum with color names as variants
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+// * Use a match expression to determine which color
+// name to print
+
+// * Use an enum with color names as variants
+enum Color {
+    Red,
+    Gold,
+    Green,
+    Indigo,
+    Cyan,
+    Magenta,
+    Purple,
+    Beige,
+    Orange,
+    Aqua,
+    RoyalBlue,
+    Yellow,
+}
+
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+fn color_type( color: Color) {
+    // * Use a match expression to determine which color
+    // name to print
+    match color {
+        Color::Red => println!("Red"),
+        Color::Gold => println!("Gold"),
+        Color::Green => println!("Green"),
+        Color::Indigo => println!("Indigo"),
+        Color::Cyan => println!("Cyan"),
+        Color::Magenta => println!("Magenta"),
+        Color::Purple => println!("Purple"),
+        Color::Beige => println!("Beige"),
+        Color::Orange => println!("Orange"),
+        Color::Aqua => println!("Aqua"),
+        Color::RoyalBlue => println!("RoyalBlue"),
+        Color::Yellow => println!("Yellow"),
+    }
+    
+}
+
+fn main() {
+    let _chosen_color = color_type(Color::Aqua);
+}
